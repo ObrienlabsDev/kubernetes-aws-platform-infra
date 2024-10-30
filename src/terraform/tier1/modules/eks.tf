55ba810ba0e8019daf1e2d3465f76173a529416b
Michael O'Brien <michael@obrienlabs.org>
2024-10-29 22:47:22 -0400

    #1 - comment node group

--- src/terraform/tier1/modules/eks.tf
+++ src/terraform/tier1/modules/eks.tf
@@ -116,7 +116,7 @@ resource "aws_subnet" "private_subnet_2" {
 }*/
 
 # Create a node group (optional)
-resource "aws_eks_node_group" "eks_node_group" {
+/*resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_cluster_role.arn # Replace with your node role ARN
@@ -133,7 +133,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   tags = {
     Name = "my-node-group"
   }
-}
+}*/
 
 # Get Availability Zones
 data "aws_availability_zones" "available" {}
